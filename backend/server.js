d3444efdb2ee818689fc85a7f9d56c01abeba5ab
Maha Uddin <Student@Admins-MacBook-Pro.local>
2025-07-19 23:43:14 -0400

    setting up api endpoints for test

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,25 @@ import express from "express";
 const app = express()
 
 app.get("/api/notes", (req, res) => {
-    res.status(200).send("you got 5 notes")
+    res.status(200).send("you got 20 notes")
 })
 
 app.post("/api/notes", (req, res) => {
-    res.status(201).send("you created a note")
+    res.status(201).json({message:"note created sucessfully"})
+
 })
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"note updated sucessfully"})
+
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"Note deleted sucessfully"})
+
+})
+
+
 
 app.listen(5001, () => {
     console.log("Server started on Port: 5001")
