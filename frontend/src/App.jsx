911589d10045d4dc6243fd700e1897d530bedc3a
Maha Uddin <Student@Admins-MacBook-Pro.local>
2025-07-21 00:34:16 -0400

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,16 @@ import toast from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>
-      <button onClick= {() => toast.error("why did you just click me, let me sleep")}>
-        Click Me
-      </button>
+    <div data-theme="valentine">
+
+<button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/Create" element={<CreatePage />} />
